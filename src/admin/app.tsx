0b82d8688927fb99dd6134158afd68227a0baa32
priscilaandreani <priscilaandreani@gmail.com>
2024-10-09 19:21:04 -0300

    chore: update admin app configuration, translations, theme and tutorials

--- src/admin/app.tsx
+++ src/admin/app.tsx
@@ -13,7 +13,29 @@ export default {
     menu: {
       logo: Logo,
     },
+    translations: {
+      en: {
+        'Auth.form.welcome.title': 'Welcome to Won Games!',
+        'Auth.form.welcome.subtitle': 'Log in to your account',
+      },
+    },
+    theme: {
+      light: {},
+      dark: {
+        colors: {
+          primary100: '#030415',
+          primary600: '#f231a5',
+          primary700: '#f231a5',
+          neutral0: '#0d102f',
+          neutral100: '#030415',
+        },
+      },
+    },
     locales: [],
+    tutorials: false,
+    notifications: {
+      releases: false,
+    },
   },
   bootstrap(app: StrapiApp) {},
 };
